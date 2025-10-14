db855bf4165df8afd715339fb5a8d4c07ffb52ac
DrOtter3000 <info@darknet-online.com>
2025-10-14 17:21:11 +0200

    make car work

--- Levels/Scripts/car.gd
+++ Levels/Scripts/car.gd
@@ -1,13 +1,14 @@
 extends Interactor
 
-var jobs_done := false
+func _ready() -> void:
+	super()
+	text = "Use Car"
 
 func interact() -> void:
-	if jobs_done:
+	var job_counter = player.tasks.get_child_count()
+	if job_counter == 0:
+		text = "Use Car"
 		queue_free()
 	else:
+		#TODO: Add other behaviour
 		print("do your stuffs")
-
-func everything_finished() -> void:
-	jobs_done = true
-	text = "Use Car"
