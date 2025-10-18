9787dd10ccc5cfbd7131eb071a472666ca47aae7
DrOtter3000 <info@darknet-online.com>
2025-10-18 19:14:25 +0200

    add repair coffee machine task

--- Levels/repair_coffee_machine.gd
+++ Levels/repair_coffee_machine.gd
@@ -1,1 +1,9 @@
-extends Area3D
+extends task_interactor
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func interact() -> void:
+	animation_player.play("repair")
+
+func turn_tv_on() -> void:
+	get_tree().call_group("TV", "tv_noise", true)
