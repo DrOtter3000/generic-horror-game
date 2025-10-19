6fa1c011624fb50c61d7712338dd0875ef046ec7
DrOtter3000 <info@darknet-online.com>
2025-10-19 20:13:11 +0200

    add fullscreen optin

--- Menus/options.gd
+++ Menus/options.gd
@@ -20,7 +20,7 @@ func _ready() -> void:
 	sensetivity_slider.value = Gamestate.mouse_sensetivity
 
 
-func  _process(delta: float) -> void:
+func  _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		_on_back_button_pressed()
 
@@ -50,3 +50,9 @@ func _on_sensetivity_slider_value_changed(value: float) -> void:
 
 func _on_sound_slider_drag_ended(value_changed: bool) -> void:
 	AudioManager.play_test_sound()
+
+func _on_fullscreen_button_pressed() -> void:
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
