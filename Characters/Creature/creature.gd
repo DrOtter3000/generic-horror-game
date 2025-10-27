77835828e8268f65723bdd2c876edfc4b5d2b281
DrOtter3000 <info@darknet-online.com>
2025-10-27 18:16:06 +0100

    add breath sfx

--- Characters/Creature/creature.gd
+++ Characters/Creature/creature.gd
@@ -1,10 +1,16 @@
 class_name Creature
 extends Node3D
 
+@export var breathing := true
+@export var breath_sound_array: Array[AudioStream]
+
+@onready var breath_player: AudioStreamPlayer3D = $BreathPlayer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 var moving := false
 var speed := 1.5
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animations: AnimationPlayer = $AnimationPlayer
 
 func leave_window() -> void:
 	rotation.y = lerp(rotation.y, -80.0, -0.1)
@@ -14,4 +20,11 @@ func leave_window() -> void:
 func _process(delta: float) -> void:
 	if moving:
 		position.z -= speed * delta
-		
+
+func play_breath_sound() -> void:
+	breath_player.stream = breath_sound_array.pick_random()
+	breath_player.play()
+
+func _on_timer_timeout() -> void:
+	if breathing:
+		play_breath_sound()
--- Levels/Scripts/go_to_sleep.gd
+++ Levels/Scripts/go_to_sleep.gd
@@ -6,6 +6,10 @@ extends task_interactor
 @onready var tv_switch_position: Node3D = $"../TVSwitchPosition"
 @onready var snore_sound_player: AudioStreamPlayer = $"../SnoreSoundPlayer"
 
+func _ready() -> void:
+	super()
+	creature.breathing = false
+
 func interact() -> void:
 	super()
 	player.working = true
@@ -14,6 +18,7 @@ func interact() -> void:
 	snore_sound_player.play()
 	await snore_sound_player.finished
 	creature.show()
+	creature.breathing = true
 	player.fade_in()
 	danger_zone.active = true
 	player.working = false
