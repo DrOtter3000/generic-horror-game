3acd013cbd7c335fb8382b077f8609bc9d7bcc5d
DrOtter3000 <info@darknet-online.com>
2025-10-29 18:48:50 +0100

    add MacOS export

--- Menus/disclaimer.gd
+++ Menus/disclaimer.gd
@@ -11,4 +11,4 @@ func _on_timer_timeout() -> void:
 	if last_message:
 		get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
 	last_message = true
-	label.text = "This game was made in 10 days by a single otter for the GameDev.tv Halloween GameJam 2025."
+	label.text = "This game was made in 10 days by a single otter for the GameDev.tv Halloween GameJam 2025. The GameJam version is still available on itch.io. In this version I only fixed a little bit of ambience."
