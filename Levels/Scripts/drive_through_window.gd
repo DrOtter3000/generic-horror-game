5657fa78340f30543b69f37c527e6b637457a109
DrOtter3000 <info@darknet-online.com>
2025-10-29 18:34:46 +0100

    add dialogue input

--- Levels/Scripts/drive_through_window.gd
+++ Levels/Scripts/drive_through_window.gd
@@ -1,11 +1,27 @@
 extends task_interactor
 
-var wait_time := 5.0
+@onready var timer: Timer = $Timer
+
+var wait_time := 2.0
 var creature: Creature
+var ready_to_continue_dialogue := false
+var dialogue_to_play := 0
+
+var dialogue_array := [
+	["You:", "W-w-what the hell is going on?"],
+	["Creature:", "Sorry for my weird look. Some people say, I would scare them."],
+	["You:", "OK, here is your purse."],
+	["Creature:", "Thank you. Oh, one more question. What is your perfume? It smells... tasty."],
+	["You:", "I don't use perfume."],
+	["Creature:", "Oh... then I have to leave. Fast... you smell tasty. See you."],
+	["You:", "See you!"]
+]
 
 func _ready() -> void:
 	super()
+	timer.wait_time = wait_time
 	creature = get_tree().get_first_node_in_group("Creature")
+	
 
 func interact() -> void:
 	super()
@@ -14,23 +30,26 @@ func interact() -> void:
 	get_tree().call_group("HUD", "deactivate_interaction_label", true)
 	get_tree().call_group("HUD", "update_communication_container", "Creature:", "Hi, I'm here to pick up my purse.")
 	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("HUD", "update_communication_container", "You:", "W-w-what the hell is going on?")
-	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("HUD", "update_communication_container", "Creature:", "Sorry for my weird look. Some people say, I would scare them.")
-	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("HUD", "update_communication_container", "You:", "OK, here is your purse.")
-	await get_tree().create_timer(wait_time/2).timeout
-	get_tree().call_group("HUD", "update_communication_container", "Creature:", "Thank you. Oh, one more question. What is your perfume? It smells... tasty.")
-	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("HUD", "update_communication_container", "You:", "I don't use perfume.")
-	await get_tree().create_timer(wait_time/2).timeout
-	get_tree().call_group("HUD", "update_communication_container", "Creature:", "Oh... then I have to leave. Fast... you smell tasty. See you.")
-	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("HUD", "update_communication_container", "You:", "See you.")
-	await get_tree().create_timer(wait_time/2).timeout
+	ready_to_continue_dialogue = true
+
+func _process(delta: float) -> void:
+	if ready_to_continue_dialogue:
+		if Input.is_action_just_pressed("jump"):
+			ready_to_continue_dialogue = false
+			get_tree().call_group("HUD", "update_communication_container", dialogue_array[dialogue_to_play][0], dialogue_array[dialogue_to_play][1])
+			dialogue_to_play += 1
+			if dialogue_to_play == dialogue_array.size():
+				quit_dialogue()
+			else:
+				timer.start()
+
+func quit_dialogue() -> void:
 	creature.leave_window()
 	get_tree().call_group("Window", "open_window", false)
 	get_tree().call_group("HUD", "deactivate_interaction_label", false)
 	get_tree().call_group("BurgerPlace", "turn_off_tv")
 	player.change_working_status(false)
 	queue_free()
+
+func _on_timer_timeout() -> void:
+	ready_to_continue_dialogue = true
--- Levels/Scripts/lost_purse.gd
+++ Levels/Scripts/lost_purse.gd
@@ -5,35 +5,49 @@ extends task_interactor
 @onready var phone_sound: AudioStreamPlayer3D = $PhoneSound
 @onready var pick_up_phone_sound = preload("res://Audio/SFX/PickUpPhone.wav")
 @onready var hang_up_phone_sound = preload("res://Audio/SFX/HangUpPhone.wav")
+@onready var timer: Timer = $Timer
 
-var wait_time := 5.0
+var wait_time := 2.0
+var ready_to_continue_dialogue := false
+var dialogue_to_play := 0
+var dialogue_array := [
+	["???:", "Hey, am I right at BurgerPiz?"],
+	["You:", "Yes, but this place is closed, I'm the janitor."],
+	["???:", "Oh, I don't want to order anything. I lost my purse, it is red. Can you have a look if one has found it?"],
+	["You:", "Yes, it is here, I can see it. You can pick it up tomorrow."],
+	["???:", "Can I pick it up right now? I'm next to the drive trough in a few seconds."],
+	["You:", "OK, why not. But please hurry up."],
+	["???:", "Thank you alot. Come to the window, I'm there right now!"]
+]
+
+func _ready() -> void:
+	super()
+	timer.wait_time = wait_time
 
 func interact() -> void:
 	super()
-	player.change_working_status(true)
-	text = ""
 	phone_sound.stop()
+	text = ""
 	phone_sound.stream = pick_up_phone_sound
 	phone_sound.play()
+	player.change_working_status(true)
 	get_tree().call_group("HUD", "deactivate_interaction_label", true)
-	get_tree().call_group("HUD", "update_communication_container", "You:", "Yes, Hello?")
-	await get_tree().create_timer(wait_time/2).timeout
-	get_tree().call_group("HUD", "update_communication_container", "???:", "Hey, am I right at BurgerPiz?")
+	get_tree().call_group("HUD", "update_communication_container", "You", "Yes, hello.")
 	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("HUD", "update_communication_container", "You:", "Yes, but this place is closed, I'm the janitor.")
-	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("HUD", "update_communication_container", "???:", "Oh, I don't want to order anything. I lost my purse, it is red. Can you have a look if one has found it?")
-	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("HUD", "update_communication_container", "You:", "Yes, it is here, I can see it. You can pick it up tomorrow.")
-	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("HUD", "update_communication_container", "???:", "Can I pick it up right now? I'm next to the drive trough in a few seconds.")
-	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("TV", "tv_noise", true, 2)
-	get_tree().call_group("HUD", "update_communication_container", "You:", "OK, why not. But please hurry up.")
-	await get_tree().create_timer(wait_time).timeout
-	get_tree().call_group("Purse", "queue_free")
-	get_tree().call_group("HUD", "update_communication_container", "???:", "Thank you alot. Come to the window, I'm there right now!")
-	await get_tree().create_timer(wait_time).timeout
+	ready_to_continue_dialogue = true
+
+func _process(delta: float) -> void:
+	if ready_to_continue_dialogue:
+		if Input.is_action_just_pressed("jump"):
+			ready_to_continue_dialogue = false
+			get_tree().call_group("HUD", "update_communication_container", dialogue_array[dialogue_to_play][0], dialogue_array[dialogue_to_play][1])
+			dialogue_to_play += 1
+			if dialogue_to_play == dialogue_array.size():
+				quit_dialogue()
+			else:
+				timer.start()
+
+func quit_dialogue() -> void:
 	get_tree().call_group("BurgerPlace", "add_drive_trough_task")
 	get_tree().call_group("HUD", "deactivate_interaction_label", false)
 	phone_sound.stream = hang_up_phone_sound
@@ -41,3 +55,6 @@ func interact() -> void:
 	await get_tree().create_timer(1).timeout
 	player.change_working_status(false)
 	queue_free()
+
+func _on_timer_timeout() -> void:
+	ready_to_continue_dialogue = true
